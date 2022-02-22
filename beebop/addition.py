d7ad247a61a3344ff1a4364a510463bc6948182f
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-02-22 13:41:23 +0000

    add simple function to be tested

---
+++ beebop/addition.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a+b
+
+
+x = 3
+y = 5
+
+result = add(x, y)
+print(result)
--- tests/test_beebop.py
+++ tests/test_beebop.py
@@ -1,5 +1,11 @@
 from beebop import __version__
+from beebop import addition
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_add():
+    assert addition.add(5, 4) == 9
+    assert addition.add(5, 10) == 15
