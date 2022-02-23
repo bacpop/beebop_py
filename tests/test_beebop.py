5759d7c9ee552167e523f828002eac07a7939965
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-02-23 13:55:14 +0000

    included testing for get_version

--- tests/test_beebop.py
+++ tests/test_beebop.py
@@ -1,5 +1,6 @@
 from beebop import __version__
 from beebop import addition
+from beebop import versions
 
 
 def test_version():
@@ -9,3 +10,8 @@ def test_version():
 def test_add():
     assert addition.add(5, 4) == 9
     assert addition.add(5, 10) == 15
+
+
+def test_get_version():
+    assert versions.get_version(["beebop"]) =="[{\"name\": \"beebop\", \"version\": \"0.1.0\"}]"
+
