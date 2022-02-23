c89b99c6fc19ceea67ed5a0be9630c0d3f699542
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-02-23 15:48:49 +0000

    Added fixtures to test flask endpoints, added schema validation

--- tests/test_beebop.py
+++ tests/test_beebop.py
@@ -1,6 +1,8 @@
 from beebop import __version__
 from beebop import addition
 from beebop import versions
+import json
+import jsonschema
 
 
 def test_version():
@@ -15,3 +17,14 @@ def test_add():
 def test_get_version():
     assert versions.get_version(["beebop"]) =="[{\"name\": \"beebop\", \"version\": \"0.1.0\"}]"
 
+
+def test_request_version(client):
+    response = client.get("/version")
+    
+    with open('spec/version.schema.json', 'r') as f:
+        schema_data = f.read()
+    schema = json.loads(schema_data)
+
+    assert jsonschema.validate(json.loads(response.data.decode("utf-8")) , schema) == None
+    assert response.data == b"[{\"name\": \"beebop\", \"version\": \"0.1.0\"}]"
+    
