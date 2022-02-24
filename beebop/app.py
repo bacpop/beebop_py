644422741a18d14eed7610043846b49f9b0a09cb
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-02-24 10:46:21 +0000

    changed response format

--- beebop/app.py
+++ beebop/app.py
@@ -5,11 +5,22 @@ from beebop import __version__, versions
 app = Flask(__name__)
 
 
+def response_success(data):
+    response = {
+        "status": "success",
+        "errors": [],
+        "data": data
+    }
+    return response
+
+
 @app.route('/version')
 def report_version():
     """
     report version of beebop (and poppunk,ska in the future)
+    wrapped in response object
     """
     vers = versions.get_version()
-    vers_json = jsonify(vers)
-    return vers_json
+    response = response_success(vers)
+    response_json = jsonify(response)
+    return response_json
--- tests/test_beebop.py
+++ tests/test_beebop.py
@@ -20,5 +20,5 @@ def test_request_version(client):
         schema_data = file.read()
     schema = json.loads(schema_data)
 
-    assert jsonschema.validate(json.loads(
-        response.data.decode("utf-8")), schema) is None
+    assert jsonschema.validate(
+        json.loads(response.data.decode("utf-8"))["data"], schema) is None
