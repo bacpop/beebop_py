5105f1b2b2c2d5d3c0b8542b9cf08444acfbcb2a
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-02-24 09:41:00 +0000

    remove addition part

--- tests/test_beebop.py
+++ tests/test_beebop.py
@@ -1,35 +1,24 @@
-'''test functions for unit and integration tests'''
-
 import json
 import jsonschema
 
 from beebop import __version__
-from beebop import addition
 from beebop import versions
 
 
 def test_version():
-    '''test the version'''
     assert __version__ == '0.1.0'
 
 
-def test_add():
-    '''test addition'''
-    assert addition.add(5, 4) == 9
-    assert addition.add(5, 10) == 15
-
-
 def test_get_version():
-    '''test function that returns version number'''
     assert versions.get_version() == [{"name": "beebop", "version": "0.1.0"}]
 
 
 def test_request_version(client):
-    '''test /version endpoint'''
     response = client.get("/version")
 
     with open('spec/version.schema.json', 'r', encoding="utf-8") as file:
         schema_data = file.read()
     schema = json.loads(schema_data)
 
-    assert jsonschema.validate(json.loads(response.data.decode("utf-8")), schema) is None
+    assert jsonschema.validate(json.loads(
+        response.data.decode("utf-8")), schema) is None
