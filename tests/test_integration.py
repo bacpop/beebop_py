2fda05b122a5e87e11bb71fc78da41fff636125d
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-02-24 12:59:07 +0000

    split tests into unit/integration, dropped version test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,17 +1,6 @@
 import json
 import jsonschema
 
-from beebop import __version__
-from beebop import versions
-
-
-def test_version():
-    assert __version__ == '0.1.0'
-
-
-def test_get_version():
-    assert versions.get_version() == [{"name": "beebop", "version": "0.1.0"}]
-
 
 def test_request_version(client):
     response = client.get("/version")
