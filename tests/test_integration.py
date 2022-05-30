756812c1fca4e893dbd835a9fa764d403ea2e01c
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-05-30 11:57:13 +0100

    added tests for poppunk endpount

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -2,6 +2,7 @@ import json
 import jsonschema
 
 import beebop.schemas
+from tests import setup
 
 
 schemas = beebop.schemas.Schema()
@@ -12,3 +13,26 @@ def test_request_version(client):
     schema = schemas.version
     assert jsonschema.validate(
         json.loads(response.data.decode("utf-8"))["data"], schema) is None
+
+
+def test_run_poppunk(client, qtbot):
+    # this requires Redis & rqworker to be running
+    # submit new job
+    response = client.post("/poppunk", json={
+        'projectHash': 'integration_test_run_poppunk',
+        'sketches': json.loads(setup.generate_json())})
+    assert response.status_code == 200
+    # retrieve job status
+    status = client.get("/status/integration_test_run_poppunk")
+    assert status.data.decode("utf-8") in ['queued', 'started', 'finished']
+    # retrieve result when finished
+
+    def status_finished():
+        status = client.get("/status/integration_test_run_poppunk")
+        assert status.data.decode("utf-8") == 'finished'
+
+    qtbot.waitUntil(status_finished, timeout=20000)
+    result = client.get("/result/integration_test_run_poppunk")
+    result_object = json.loads(result.data.decode("utf-8"))
+    assert result_object["status"] == "success"
+    assert jsonschema.validate(result_object["data"], schemas.cluster) is None
--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,8 +1,10 @@
 import jsonschema
 import json
+from PopPUNK import __version__ as poppunk_version
 
 from beebop import __version__ as beebop_version
 from beebop import versions
+from beebop import assignClusters
 import beebop.schemas
 from tests import setup
 
@@ -11,9 +13,24 @@ schemas = beebop.schemas.Schema()
 
 def test_get_version():
     assert versions.get_version() == [
-        {"name": "beebop", "version": beebop_version}]
+        {"name": "beebop", "version": beebop_version},
+        {"name": "poppunk", "version": poppunk_version}]
+    assert jsonschema.validate(versions.get_version(),
+                               schemas.version) is None
 
 
 def test_setup():
     assert jsonschema.validate(json.loads(
-        setup.generate_json()), schemas.sketch) is None
+        setup.generate_json()), schemas.sketches) is None
+
+
+def test_poppunk_assign():
+    assert assignClusters.get_clusters(
+        [
+            '02ff334f17f17d775b9ecd69046ed296',
+            '9c00583e2f24fed5e3c6baa87a4bfa4c',
+            '99965c83b1839b25c3c27bd2910da00a'
+        ], 'unit_test_poppunk_assign') == {
+            0: {'cluster': 9, 'hash': '02ff334f17f17d775b9ecd69046ed296'},
+            1: {'cluster': 10, 'hash': '99965c83b1839b25c3c27bd2910da00a'},
+            2: {'cluster': 41, 'hash': '9c00583e2f24fed5e3c6baa87a4bfa4c'}}
