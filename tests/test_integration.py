24c5c209e2a8f65eb359f22fbcb3a8af051d907e
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-06-13 13:01:03 +0100

    reduced timeout, added schema validation for sketches

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,6 +1,5 @@
 import json
 import jsonschema
-
 import beebop.schemas
 from tests import setup
 
@@ -17,10 +16,13 @@ def test_request_version(client):
 
 def test_run_poppunk(client, qtbot):
     # this requires Redis & rqworker to be running
+    # generate sketches
+    sketches = json.loads(setup.generate_json())
+    assert jsonschema.validate(sketches, schemas.sketches) is None
     # submit new job
     response = client.post("/poppunk", json={
         'projectHash': 'integration_test_run_poppunk',
-        'sketches': json.loads(setup.generate_json())})
+        'sketches': sketches})
     assert response.status_code == 200
     # retrieve job status
     status = client.get("/status/integration_test_run_poppunk")
@@ -33,7 +35,7 @@ def test_run_poppunk(client, qtbot):
         status = client.get("/status/integration_test_run_poppunk")
         assert json.loads(status.data.decode("utf-8"))['data'] == 'finished'
 
-    qtbot.waitUntil(status_finished, timeout=2000000)
+    qtbot.waitUntil(status_finished, timeout=20000)
     result = client.get("/result/integration_test_run_poppunk")
     result_object = json.loads(result.data.decode("utf-8"))
     assert result_object["status"] == "success"
