00833e0e868eb0ab43f1e8aa6133463e6e66e5f2
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-07-14 13:19:25 +0100

    adadpted fct arguments, use rwapper to hide long poppunk fct calls

---
+++ beebop/poppunkWrapper.py
@@ -0,0 +1,83 @@
+from PopPUNK.visualise import generate_visualisations
+
+
+class PoppunkWrapper:
+    def __init__(self, fs, db_paths, args, p_hash):
+        self.fs = fs
+        self.db_paths = db_paths
+        self.args = args
+        self.p_hash = p_hash
+
+    def create_microreact(self, cluster_no):
+        generate_visualisations(
+            query_db=self.fs.output(self.p_hash),
+            ref_db=self.db_paths.db,
+            distances=self.fs.distances(self.p_hash),
+            rank_fit=None,
+            threads=self.args.visualise.threads,
+            output=self.fs.output_microreact(self.p_hash, cluster_no),
+            gpu_dist=self.args.visualise.gpu_dist,
+            deviceid=self.args.visualise.deviceid,
+            external_clustering=self.args.visualise.external_clustering,
+            microreact=True,
+            phandango=self.args.visualise.phandango,
+            grapetree=self.args.visualise.grapetree,
+            cytoscape=self.args.visualise.cytoscape,
+            perplexity=self.args.visualise.perplexity,
+            strand_preserved=self.args.visualise.strand_preserved,
+            include_files=self.fs.include_files(self.p_hash, cluster_no),
+            model_dir=self.db_paths.db,
+            previous_clustering=self.db_paths.previous_clustering,
+            previous_query_clustering=(
+                self.fs.previous_query_clustering(self.p_hash)),
+            previous_mst=None,
+            previous_distances=None,
+            network_file=self.fs.network_file(self.p_hash),
+            gpu_graph=self.args.visualise.gpu_graph,
+            info_csv=self.args.visualise.info_csv,
+            rapidnj=self.args.visualise.rapidnj,
+            tree=self.args.visualise.tree,
+            mst_distances=self.args.visualise.mst_distances,
+            overwrite=self.args.visualise.overwrite,
+            core_only=self.args.visualise.core_only,
+            accessory_only=self.args.visualise.accessory_only,
+            display_cluster=self.args.visualise.display_cluster,
+            web=True
+        )
+
+    def create_network(self):
+        generate_visualisations(
+            query_db=self.fs.output(self.p_hash),
+            ref_db=self.db_paths.db,
+            distances=self.fs.distances(self.p_hash),
+            rank_fit=None,
+            threads=self.args.visualise.threads,
+            output=self.fs.output_network(self.p_hash),
+            gpu_dist=self.args.visualise.gpu_dist,
+            deviceid=self.args.visualise.deviceid,
+            external_clustering=self.args.visualise.external_clustering,
+            microreact=self.args.visualise.microreact,
+            phandango=self.args.visualise.phandango,
+            grapetree=self.args.visualise.grapetree,
+            cytoscape=True,
+            perplexity=self.args.visualise.perplexity,
+            strand_preserved=self.args.visualise.strand_preserved,
+            include_files=None,
+            model_dir=self.db_paths.db,
+            previous_clustering=self.db_paths.previous_clustering,
+            previous_query_clustering=(
+                self.fs.previous_query_clustering(self.p_hash)),
+            previous_mst=None,
+            previous_distances=None,
+            network_file=self.fs.network_file(self.p_hash),
+            gpu_graph=self.args.visualise.gpu_graph,
+            info_csv=self.args.visualise.info_csv,
+            rapidnj=self.args.visualise.rapidnj,
+            tree="nj",
+            mst_distances=self.args.visualise.mst_distances,
+            overwrite=self.args.visualise.overwrite,
+            core_only=self.args.visualise.core_only,
+            accessory_only=self.args.visualise.accessory_only,
+            display_cluster=self.args.visualise.display_cluster,
+            web=True
+        )
--- beebop/visualise.py
+++ beebop/visualise.py
@@ -1,114 +1,48 @@
 from PopPUNK.visualise import generate_visualisations
 from rq import get_current_job
 from redis import Redis
+from beebop.poppunkWrapper import PoppunkWrapper
 
 
-def microreact(p_hash, outdir, db_paths, args):
+def microreact(p_hash, fs, db_paths, args):
     """
     generate files to use with microreact.
     Output files are .csv, .dot and .nwk
     (last one only for clusters with >3 isolates)
 
     p_hash: project hash to find input data (output from assignClusters)
-    outdir: directory where input data is stored
+    fs: PoppunkFilestore with paths to input data
     db_paths: location of database
     args: arguments for poppunk functions
     """
 
     # get results from previous job
-    redis = Redis()
-    current_job = get_current_job(redis)
-    prev_job = current_job.dependency
-    prev_result = prev_job.result
+    current_job = get_current_job(Redis())
+    assign_result = current_job.dependency.result
+    microreact_internal(assign_result, p_hash, fs, db_paths, args)
 
+
+def microreact_internal(assign_result, p_hash, fs, db_paths, args):
+    wrapper = PoppunkWrapper(fs, db_paths, args, p_hash)
     queries_clusters = []
-    for item in prev_result.values():
+    for item in assign_result.values():
         queries_clusters.append(item['cluster'])
-
     for cluster_no in set(queries_clusters):
-        generate_visualisations(
-            query_db=outdir,
-            ref_db=db_paths.db,
-            distances=outdir + '/' + p_hash + '.dists',
-            rank_fit=None,
-            threads=args.visualise.threads,
-            output=outdir + '/microreact_' + str(cluster_no),
-            gpu_dist=args.visualise.gpu_dist,
-            deviceid=args.visualise.deviceid,
-            external_clustering=args.visualise.external_clustering,
-            microreact=True,
-            phandango=args.visualise.phandango,
-            grapetree=args.visualise.grapetree,
-            cytoscape=args.visualise.cytoscape,
-            perplexity=args.visualise.perplexity,
-            strand_preserved=args.visualise.strand_preserved,
-            include_files=outdir + "/include" + str(cluster_no) + ".txt",
-            model_dir=db_paths.db,
-            previous_clustering=(db_paths.db + '/' +
-                                 db_paths.name + '_clusters.csv'),
-            previous_query_clustering=outdir + '/' + p_hash + '_clusters.csv',
-            previous_mst=None,
-            previous_distances=None,
-            network_file=outdir + "/" + p_hash + "_graph.gt",
-            gpu_graph=args.visualise.gpu_graph,
-            info_csv=args.visualise.info_csv,
-            rapidnj=args.visualise.rapidnj,
-            tree=args.visualise.tree,
-            mst_distances=args.visualise.mst_distances,
-            overwrite=args.visualise.overwrite,
-            core_only=args.visualise.core_only,
-            accessory_only=args.visualise.accessory_only,
-            display_cluster=args.visualise.display_cluster,
-            web=True
-        )
+        wrapper.create_microreact(cluster_no)
 
 
-def network(p_hash, outdir, db_paths, args):
+def network(p_hash, fs, db_paths, args):
     """
     generate files to draw a network.
     Output files are .graphml and .csv
 
     p_hash: project hash to find input data (output from assignClusters)
-    outdir: directory where input data is stored
+    fs: PoppunkFilestore with paths to input data
     db_paths: location of database
     args: arguments for poppunk functions
 
     Currently poppunk does not allow to subset isolates in this mode.
     Ideally we'd want to only display clusters that have a new isolate added.
     """
-
-    generate_visualisations(
-        query_db=outdir,
-        ref_db=db_paths.db,
-        distances=outdir + '/' + p_hash + '.dists',
-        rank_fit=None,
-        threads=args.visualise.threads,
-        output=outdir + '/network',
-        gpu_dist=args.visualise.gpu_dist,
-        deviceid=args.visualise.deviceid,
-        external_clustering=args.visualise.external_clustering,
-        microreact=args.visualise.microreact,
-        phandango=args.visualise.phandango,
-        grapetree=args.visualise.grapetree,
-        cytoscape=True,
-        perplexity=args.visualise.perplexity,
-        strand_preserved=args.visualise.strand_preserved,
-        include_files=None,
-        model_dir=db_paths.db,
-        previous_clustering=(db_paths.db +
-                             '/' + db_paths.name + '_clusters.csv'),
-        previous_query_clustering=outdir + '/' + p_hash + '_clusters.csv',
-        previous_mst=None,
-        previous_distances=None,
-        network_file=outdir + "/" + p_hash + "_graph.gt",
-        gpu_graph=args.visualise.gpu_graph,
-        info_csv=args.visualise.info_csv,
-        rapidnj=args.visualise.rapidnj,
-        tree="nj",
-        mst_distances=args.visualise.mst_distances,
-        overwrite=args.visualise.overwrite,
-        core_only=args.visualise.core_only,
-        accessory_only=args.visualise.accessory_only,
-        display_cluster=args.visualise.display_cluster,
-        web=True
-    )
+    wrapper = PoppunkWrapper(fs, db_paths, args, p_hash)
+    wrapper.create_network()
