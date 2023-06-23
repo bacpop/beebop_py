23296a31cd01b5b3091379e5628d71f1cdf4cc35
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2023-06-23 13:31:12 +0100

    fix assert

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -289,7 +289,7 @@ def test_get_project_returns_404_if_unknown_project_hash(client):
     data = read_data(result)["data"]
     assert data is None
     errors = read_data(result)["errors"]
-    assert errors[0] = {
+    assert errors[0] == {
         "error": "Project hash not found",
         "detail": "Project has does not have an associated job"
     }
