82e89343217147d42d440d3bbf2e7b7e0a7b17f8
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2024-03-09 10:16:32 +0000

    prefix cluster

--- beebop/utils.py
+++ beebop/utils.py
@@ -203,7 +203,7 @@ def get_external_clusters_from_file(external_clusters_file: str,
                     numeric_clusters = [int(x) for x in clusters]
                     numeric_clusters.sort()
                     print("Setting cluster {} for sample {}", numeric_clusters[0], sample_id)
-                    result[sample_id] = numeric_clusters[0]
+                    result[sample_id] = "GPSC{}".format(numeric_clusters[0])
 
                 # Remove sample id from remaining hashes to find
                 remaining_hashes.remove(sample_id)
