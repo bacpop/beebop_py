5dfb5c73522fac627c9095eeeb0350baf70c4fc4
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2024-03-09 15:58:40 +0000

    try actually setting external clustering

--- beebop/poppunkWrapper.py
+++ beebop/poppunkWrapper.py
@@ -111,7 +111,7 @@ class PoppunkWrapper:
         previous assign_clusters.get_clusters() output.]
         """
         sys.stderr.write("Generating network with:\n")
-        sys.stderr.write("prev_clustering: {}\n".format(self.args.visualise.external_clustering_prev))
+        sys.stderr.write("prev_clustering: {}\n".format(self.args.visualise.external_clustering))
         sys.stderr.write("previous_query_clustering: {}\n".format(self.fs.previous_query_clustering(self.p_hash)))
         generate_visualisations(
             query_db=self.fs.output(self.p_hash),
@@ -133,7 +133,7 @@ class PoppunkWrapper:
             include_files=None,
             model_dir=self.db_paths.db,
             #previous_clustering=self.db_paths.previous_clustering,
-            previous_clustering=self.args.visualise.external_clustering_prev,
+            previous_clustering=self.args.visualise.external_clustering,
             #previous_clustering=None,
             previous_query_clustering=(
                 self.fs.previous_query_clustering(self.p_hash)),
