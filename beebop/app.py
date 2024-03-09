ce04ad698c61d51e5a83b730b3457dc9442e4363
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2024-03-09 19:20:17 +0000

    strip out cluster prefix when fetching graphml

--- beebop/app.py
+++ beebop/app.py
@@ -485,7 +485,8 @@ def download_graphml_internal(p_hash: str,
     try:
         with open(fs.network_mapping(p_hash), 'rb') as dict:
             cluster_component_mapping = pickle.load(dict)
-        component = cluster_component_mapping[str(cluster)]
+        # TODO: use util to get cluster number
+        component = cluster_component_mapping[cluster.replace("GPSC", "")]
         path = fs.network_output_component(p_hash, component)
         with open(path, 'r') as graphml_file:
             graph = graphml_file.read()
