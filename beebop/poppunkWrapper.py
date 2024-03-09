62f892757435f04486cbb5669ed70e162448b425
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2024-03-09 14:16:24 +0000

    confirm files

--- beebop/poppunkWrapper.py
+++ beebop/poppunkWrapper.py
@@ -109,6 +109,9 @@ class PoppunkWrapper:
         [Generates network visualisation output in .graphml format based on
         previous assign_clusters.get_clusters() output.]
         """
+        sys.stderr.write("Generating network with:\n")
+        sys.stderr.write("external_clustering: {}\n".format(self.args.visualise.external_clustering)
+        sys.stderr.write("previous_query_clustering: {}\n".format(self.fs.previous_query_clustering(self.p_hash))
         generate_visualisations(
             query_db=self.fs.output(self.p_hash),
             ref_db=self.db_paths.db,
