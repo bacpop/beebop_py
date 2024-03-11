7e7ed6c3e5fbfdafb57c76cdc25ec7a73a244236
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2024-03-11 11:21:10 +0000

    fs as param to util

--- beebop/app.py
+++ beebop/app.py
@@ -66,7 +66,8 @@ def check_connection(redis) -> None:
         abort(500, description="Redis not found")
 
 
-def poppunk_cluster_from_external_cluster(external_cluster: str) -> str:
+def poppunk_cluster_from_external_cluster(fs: PoppunkFileStore,
+                                          external_cluster: str) -> str:
     with open(fs.external_to_poppunk_clusters(p_hash), 'rb') as dict:
          external_to_poppunk_clusters = pickle.load(dict)
     return external_to_poppunk_clusters[external_cluster]
@@ -89,7 +90,7 @@ def generate_zip(fs: PoppunkFileStore,
     :return BytesIO: [memory file]
     """
     memory_file = BytesIO()
-    internal_cluster = poppunk_cluster_from_external_cluster(str(cluster))
+    internal_cluster = poppunk_cluster_from_external_cluster(fs, str(cluster))
     if type == 'microreact':
         path_folder = fs.output_microreact(p_hash, internal_cluster)
         add_files(memory_file, path_folder)
@@ -494,7 +495,7 @@ def download_graphml_internal(p_hash: str,
     try:
         with open(fs.network_mapping(p_hash), 'rb') as dict:
             cluster_component_mapping = pickle.load(dict)
-        internal_cluster = poppunk_cluster_from_external_cluster(cluster)
+        internal_cluster = poppunk_cluster_from_external_cluster(fs, cluster)
         component = cluster_component_mapping[internal_cluster]
         path = fs.network_output_component(p_hash, component)
         with open(path, 'r') as graphml_file:
