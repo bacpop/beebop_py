da14b953bed710a4439945e07f2ffc17254661fc
anmol thapar <mr.anmolthapar@gmail.com>
2024-09-10 07:57:55 +0100

    Refactor clean_external_clusters_csv.py script to remove GPSC column and rename merge_history column to Cluster

---
+++ scripts/clean_external_clusters_csv.py
@@ -0,0 +1,20 @@
+"""
+This python script takes in the original external_clusters.csv file and cleans it. 
+It removes the GPSC column and rename merge_history column to Cluster.
+
+To run script type the following: `python3 clean_external_clusters_csv`
+"""
+
+import pandas as pd
+
+file_path = "./beebop/resources/GPS_v9_external_clusters.csv"
+df = pd.read_csv(file_path)
+
+# Remove the "GPSC" column
+df.drop("GPSC", axis=1, inplace=True)
+
+# Rename the "merge_history" column to "Cluster"
+df.rename(columns={"merge_history": "Cluster"}, inplace=True)
+
+# Save the updated data to a new CSV file
+df.to_csv(file_path, index=False)
