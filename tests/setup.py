ec4692ef88676b052a71b56f210c8646e55a14c3
anmol thapar <mr.anmolthapar@gmail.com>
2024-10-08 12:26:55 +0100

    Refactor database file path in tests/setup.py

--- tests/setup.py
+++ tests/setup.py
@@ -39,7 +39,7 @@ name_mapping = {
     "9c00583e2f24fed5e3c6baa87a4bfa4c": "name2.fa"
 }
 
-db_fs = DatabaseFileStore('./storage/GPS_v8_ref',
+db_fs = DatabaseFileStore('./storage/dbs/GPS_v8_ref',
                           "GPS_v8_external_clusters.csv")
 args = get_args()
 species = "Streptococcus pneumoniae"
