d56c9ca13f6ee4ec067da5b02d61590582e3deaf
anmol thapar <mr.anmolthapar@gmail.com>
2025-01-16 08:56:46 +0000

    unit tests

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -177,11 +177,10 @@ def test_microreact(mocker):
 
 
 @patch("beebop.visualise.replace_filehashes")
-def test_microreact_per_cluster(mock_replace_filehashes, mocker):
+def test_microreact_per_cluster(mock_replace_filehashes):
     p_hash = "unit_test_microreact_internal"
     cluster = "GPSC16"
     wrapper = Mock()
-    mocker.patch.object(wrapper, "create_microreact")
 
     visualise.microreact_per_cluster(
         cluster,
@@ -196,6 +195,30 @@ def test_microreact_per_cluster(mock_replace_filehashes, mocker):
     mock_replace_filehashes.assert_called_with(
         fs.output_microreact(p_hash, 16), setup.name_mapping
     )
+
+
+@patch("beebop.visualise.replace_filehashes")
+@patch("os.remove")
+def test_microreact_per_cluster_last_cluster(mock_remove, mock_replace_filehashes):
+    p_hash = "unit_test_microreact_internal"
+    cluster = "GPSC16"
+    wrapper = Mock()
+
+    visualise.microreact_per_cluster(
+        cluster,
+        p_hash,
+        fs,
+        wrapper,
+        setup.name_mapping,
+        external_to_poppunk_clusters,
+        True # is_last_cluster_to_process
+    )
+
+    wrapper.create_microreact.assert_called_with("16", "9")
+    mock_replace_filehashes.assert_called_with(
+        fs.output_microreact(p_hash, 16), setup.name_mapping
+    )
+    mock_remove.assert_called_with(fs.tmp_output_metadata(p_hash))
 
 
 def test_queue_microreact_jobs(mocker):
@@ -289,8 +312,8 @@ def test_network_internal():
             + f"/network_component_{cluster_num}.graphml"
         )
 
-
-def test_run_poppunk_internal(qtbot):
+@patch("beebop.app.add_amr_to_metadata")
+def test_run_poppunk_internal(mock_add_amr_metadata, qtbot):
     fs_json = FileStore("./tests/files/json")
     sketches = {
         "e868c76fec83ee1f69a95bd27b8d5e76": fs_json.get(
@@ -313,6 +336,7 @@ def test_run_poppunk_internal(qtbot):
         redis,
         queue,
         setup.species,
+        [{"ID": "ID1", "AMR": "AMR1"}]
     )
     job_ids = read_data(response)["data"]
     # stores sketches in storage
@@ -354,6 +378,10 @@ def test_run_poppunk_internal(qtbot):
     assert (
         read_redis("beebop:hash:job:network", project_hash, redis)
         == job_ids["network"]
+    )
+    
+    mock_add_amr_metadata.assert_called_once_with(
+        fs, project_hash, [{"ID": "ID1", "AMR": "AMR1"}], setup.ref_db_fs.metadata
     )
 
 
@@ -1648,3 +1676,87 @@ def test_setup_db_file_stores_fulldb_missing(mock_exists):
     # Verify ref database path used
     assert ref_db_fs.db == f"{app.dbs_location}/ref_database"
     assert full_db_fs.db == f"{app.dbs_location}/ref_database"
+
+
+def test_add_amr_to_metadata_no_init_metadata(tmp_path):
+    fs = Mock()
+    fs.tmp_output_metadata.return_value = str(tmp_path / "tmp_output_metadata.csv")
+    amr_metadata = [
+        {"ID": "sample1", "AMR": "AMR1"},
+        {"ID": "sample2", "AMR": "AMR2"},
+    ]
+    p_hash = "hash"
+
+    app.add_amr_to_metadata(fs, p_hash, amr_metadata)
+
+    res = pd.read_csv(tmp_path / "tmp_output_metadata.csv")
+    fs.tmp_output_metadata.assert_called_once_with(p_hash)
+    len(res) == 2
+    assert res.loc["ID"].tolist() == ["sample1", "sample2"]
+    assert res.loc["AMR"].tolist() == ["AMR1", "AMR2"]
+
+
+def test_add_amr_to_metadata_init_metadata(tmp_path):
+    fs = Mock()
+    fs.tmp_output_metadata.return_value = str(tmp_path / "tmp_output_metadata.csv")
+    metadata = pd.DataFrame(
+        {
+            "ID": ["sample1", "sample2"],
+            "AMR": ["AMR1", "AMR2"],
+        }
+    )
+    metadata.to_csv(tmp_path / "metadata.csv", index=False)
+    amr_metadata = [
+        {"ID": "sample3", "AMR": "AMR3"},
+        {"ID": "sample4", "AMR": "AMR4"},
+    ]
+    metadata_file = tmp_path / "metadata.csv"
+    p_hash = "hash"
+
+    app.add_amr_to_metadata(fs, p_hash, amr_metadata, metadata_file)
+
+    res = pd.read_csv(tmp_path / "tmp_output_metadata.csv")
+    print(res)
+    fs.tmp_output_metadata.assert_called_once_with(p_hash)
+    len(res) == 4
+    assert res["ID"].tolist() == ["sample1", "sample2", "sample3", "sample4"]
+    assert res["AMR"].tolist() == ["AMR1", "AMR2", "AMR3", "AMR4"]
+
+def test_update_microreact_json():
+    json_microreact = {
+        "meta": {
+            "name": "Old Title"
+        },
+        "tables": {
+            "table-1": {
+                "columns": [{"field": "ID"}]
+            }
+        }
+    }
+    cluster_num = "123"
+
+    app.update_microreact_json(json_microreact, cluster_num)
+
+    # Check title gets updated with correct format
+    assert json_microreact["meta"]["name"].startswith(f"Cluster {cluster_num} - ")
+    assert ":" in json_microreact["meta"]["name"] # Check datetime got added
+    
+    # Check expected columns were added
+    expected_columns = [
+        {"field": "ID"},
+        {"field": "Status", "width": 103, "sort": "asc"},
+        {"field": "Penicillin Resistance", "width": 183},
+        {"field": "Chloramphenicol Resistance", "width": 233}, 
+        {"field": "Erythromycin Resistance", "width": 209},
+        {"field": "Tetracycline Resistance", "width": 202},
+        {"field": "Cotrim Resistance", "width": 169}
+    ]
+
+    assert json_microreact["tables"]["table-1"]["columns"] == expected_columns
+
+def test_tmp_output_metadata(tmp_path):
+    fs = PoppunkFileStore(tmp_path)
+    
+    result = fs.tmp_output_metadata("hash")
+    
+    assert result == str(PurePath(fs.tmp("hash"), "metadata.csv"))
